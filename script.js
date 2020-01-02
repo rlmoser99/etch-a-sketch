d861b52680dc7280c8a5ca75622e717dc13dfb00
Rachel Moser <moser.rachel@gmail.com>
2020-01-02 14:58:19 -0600

    add size button logic

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
-// Container & Grid Set-Up
+// Container & Grid Set-Up 
 const container = document.querySelector('.container');
+const defaultNumber = 4;
+let gridArea = defaultNumber * defaultNumber;
+container.style.gridTemplateColumns = `repeat(${defaultNumber}, 1fr)`;
+container.style.gridTemplateRows = `repeat(${defaultNumber}, 1fr)`;
 
-function addGridItem (j) {
+function addGridItem (j) { 
     for (let i = 1; i <= j; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid');
+        // gridItem.classList.add(`${i}`);
         container.insertAdjacentElement('beforeend', gridItem);
     }
 }
 
-addGridItem(16);
+addGridItem(gridArea);
 
 // Modifying Grid Item Color
 
-const gridItems = document.querySelectorAll('.grid');
+let gridItems = document.querySelectorAll('.grid');
 
 function blackColor(    ) {
     this.style.backgroundColor = '#000000';
 }
 
 gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', blackColor))
+
+// Size Button
+
+sizeButton = document.querySelector('.size');
+
+function sizePrompt () {
+    gridItems.forEach(gridItem => gridItem.remove());
+    userNumber = prompt('How many squares would you like on each side?')
+    addGridItem(userNumber * userNumber);
+    container.style.gridTemplateColumns = `repeat(${userNumber}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${userNumber}, 1fr)`;
+    gridItems = document.querySelectorAll('.grid');
+    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', blackColor))
+}
+
+sizeButton.addEventListener('click', sizePrompt);
 
 // Clear Button
 clearButton = document.querySelector('.refresh');
