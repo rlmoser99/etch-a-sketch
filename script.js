7c2f2f796c4e16016c278e2c4474222bda2a6212
Rachel Moser <moser.rachel@gmail.com>
2020-01-02 13:07:14 -0600

    add clear/refresh button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Container & Grid Set-Up
 const container = document.querySelector('.container');
-
 
 function addGridItem (j) {
     for (let i = 1; i <= j; i++) {
@@ -11,6 +11,8 @@ function addGridItem (j) {
 
 addGridItem(16);
 
+// Modifying Grid Item Color
+
 const gridItems = document.querySelectorAll('.grid');
 
 function blackColor(    ) {
@@ -18,3 +20,12 @@ function blackColor(    ) {
 }
 
 gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', blackColor))
+
+// Clear Button
+clearButton = document.querySelector('.refresh');
+
+function refreshPage() {
+    window.location.reload(true);
+}
+
+clearButton.addEventListener('click', refreshPage);
