831a58a49b2b447371911575ccdb4c773334222c
Rachel Moser <moser.rachel@gmail.com>
2020-01-03 14:01:01 -0600

    refactor code to 1 grid creating function

--- script.js
+++ script.js
@@ -1,62 +1,52 @@
-// Container & Grid Set-Up 
+// Page Set-Up 
 const container = document.querySelector('.container');
-const defaultNumber = 4;
-let gridArea = defaultNumber * defaultNumber;
-container.style.gridTemplateColumns = `repeat(${defaultNumber}, 1fr)`;
-container.style.gridTemplateRows = `repeat(${defaultNumber}, 1fr)`;
+// let gridItems = document.querySelectorAll('.grid');
+const sizeButton = document.querySelector('.size');
+const clearButton = document.querySelector('.clear');
 
-function addGridItem (j) { 
-    for (let i = 1; i <= j; i++) {
+function createGrid (width, area) { 
+    for (let i = 1; i <= area; i++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid');
-        // gridItem.classList.add(`${i}`);
+        gridItem.classList.add(`${i}`);
+        container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${width}, 1fr)`;
         container.insertAdjacentElement('beforeend', gridItem);
     }
+    let gridItems = document.querySelectorAll('.grid');
+    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', colorGrid))
 }
 
-addGridItem(gridArea);
+// Create default grid on page load
+createGrid(4, 16);
 
-// Modifying Grid Item Color
-
-let gridItems = document.querySelectorAll('.grid');
-
-function blackColor() {
+// Coloring the Grid 
+function colorGrid() {
     this.style.backgroundColor = '#000000';
 }
 
-gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', blackColor))
-
-// Size Button
-
-sizeButton = document.querySelector('.size');
-
 function sizePrompt () {
+    let gridItems = document.querySelectorAll('.grid');
     gridItems.forEach(gridItem => gridItem.remove());
     userNumber = prompt('How many squares would you like on each side?')
-    addGridItem(userNumber * userNumber);
-    container.style.gridTemplateColumns = `repeat(${userNumber}, 1fr)`;
-    container.style.gridTemplateRows = `repeat(${userNumber}, 1fr)`;
-    gridItems = document.querySelectorAll('.grid');
-    gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', blackColor))
+    userArea = userNumber * userNumber;
+    createGrid(userNumber, userArea)
 }
 
-sizeButton.addEventListener('click', sizePrompt);
-
-// Clear Button
-clearButton = document.querySelector('.refresh');
-
-function refreshPage() {
-    window.location.reload(true);
+function eraseColor() {
+    let gridItems = document.querySelectorAll('.grid');
+    gridItems.forEach(gridItem => gridItem.style.backgroundColor = '#ffffff');
 }
-
-clearButton.addEventListener('click', refreshPage);
 
 // Rainbow
-colorButtons = document.querySelectorAll('.color-choice');
+// colorButtons = document.querySelectorAll('.color-choice');
 
-function whatColor(e) {
-    // this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    console.log(e.target.dataset.color);
-}
+// function whatColor(e) {
+//     // this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+//     console.log(e.target.dataset.color);
+// }
 
+// Event Listeners
 colorButtons.forEach(colorButton => colorButton.addEventListener('click', whatColor))
+sizeButton.addEventListener('click', sizePrompt);
+clearButton.addEventListener('click', eraseColor);
