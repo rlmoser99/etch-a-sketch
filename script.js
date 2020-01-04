ee8a54d8b7e2ae40763b8c142c4e5d1293c272f3
Rachel Moser <moser.rachel@gmail.com>
2020-01-04 12:52:32 -0600

    updated color buttons

--- script.js
+++ script.js
@@ -3,36 +3,89 @@ const container = document.querySelector('.container');
 const sizeButton = document.querySelector('.size');
 const clearButton = document.querySelector('.clear');
 const gridColor = document.querySelector('.grid-color')
-const classColors = ['grid-color', 'black', 'rainbow'];
+const classColors = ['grid-color', 'pixel-black', 'pixel-rainbow', 'pixel-gray'];
+let gridNumber = 4
+let gridArea = 16;
 
-function createGrid (width, area) { 
-    for (let i = 1; i <= area; i++) {
+function createGrid (gridNumber, gridArea, color) { 
+    for (let i = 1; i <= gridArea; i++) {
         let gridItem = document.createElement('div');
-        container.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
-        container.style.gridTemplateRows = `repeat(${width}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+        container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
         container.insertAdjacentElement('beforeend', gridItem);
     } 
     let gridPixels = container.querySelectorAll('div');
-    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid))
+    gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid(color)))
 }
 
 // Create default grid on page load
-createGrid(4, 16);
+createGrid(gridNumber, gridArea, 'default');
 
 // Coloring the Grid - Make a function & css class for each color
-function colorGrid() {
+// function colorGrid() {
+    //     console.log(this);
+    //     this.classList.add('pixel-black');
+    // }
+    
+function colorGrid(color) {
+    // console.log(color)
+    let gridPixels = container.querySelectorAll('div');
+    if (color === 'default') {
+        console.log('default is the color')
+        gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', blackPixels))
+    } else if (color === 'black'){
+        console.log('black is the color BUTTON')
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', rainbowPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', grayPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', blackPixels)) 
+    } else if (color === 'rainbow'){
+        // console.log('not black');
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', blackPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', grayPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', rainbowPixels)) 
+    } else {
+        // console.log('not black');
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', blackPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.removeEventListener('mouseover', grayPixels)) 
+        gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', grayPixels)) 
+    }
+    // this.classList.add(`pixel-${color}`);
+}
+
+// function colorGrid(color) {
     // gridItems.forEach(gridItem => gridItem.addEventListener('mouseover', colorGrid))
     // Need do remove classes from grid Item gridItem.classList.remove(...cls);
-    this.classList.add('grid-color');
+    // this.classList.add('grid-color');
     // console.log('new color needed')
+    // if (color = 'black') {
+        // console.log('default is needed')
+        // this.classList.add('pixel-black');
+    // } else {
+        // this.classList.add('pixel-rainbow')
+    // }
+// }
+
+function blackPixels() {
+    this.classList.remove(...classColors);
+    this.classList.add('pixel-black');
+}
+
+function rainbowPixels() {
+    this.classList.remove(...classColors);
+    this.classList.add('pixel-rainbow');
+}
+
+function grayPixels() {
+    this.classList.remove(...classColors);
+    this.classList.add('pixel-gray');
 }
 
 function sizePrompt () {
     let gridPixels = container.querySelectorAll('div');
     gridPixels.forEach(gridPixel => gridPixel.remove());
-    userNumber = prompt('How many squares would you like on each side?')
-    userArea = userNumber * userNumber;
-    createGrid(userNumber, userArea)
+    gridNumber = prompt('How many squares would you like on each side?')
+    gridArea = gridNumber * gridNumber;
+    createGrid(gridNumber, gridArea,)
 }
 
 function eraseColor() {
@@ -43,29 +96,28 @@ function eraseColor() {
 // Color Choices
 colorButtons = document.querySelectorAll('.color-choice');
 
-function updateColor(e) {
+function updateColor(event) {
     // this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    console.log(e.target.dataset.color);
-    switch (e.target.dataset.color) {
+    // console.log(e.target.dataset.color);
+    switch (event.target.dataset.color) {
+    // switch (event) {
         case 'black':
-            console.log('black was chosen');
-            gridColor.style.backgroundColor = '#000000';
+            // console.log('black was chosen');
+            createGrid(gridNumber, gridArea, 'black')
             break;
         case 'rainbow':
-            console.log('rainbow was chosen');
-            gridColor.style.backgroundColor = '#FF0000';
+            // console.log('rainbow was chosen');
+            createGrid(gridNumber, gridArea, 'rainbow')
             break;
         case 'gray':
-            console.log('grey was chosen');
-            gridColor.style.backgroundColor = '#808080';
+            console.log('gray was chosen');
+            createGrid(gridNumber, gridArea, 'gray')
             break;
         case 'eraser':
             console.log('eraser was chosen');
-            gridColor.style.backgroundColor = '#0000FF';
             break;
         default:
             console.log('default was given');
-            gridColor.style.backgroundColor = '#000000';
             break;
     }    
 }
